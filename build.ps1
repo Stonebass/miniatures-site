a79c2762e87683051d497f769e04c5cc7201d6fe
Johan Stenberg <johanhenrystenberg@hotmail.com>
2018-09-26 22:12:48 +0200

    added som features to build script

--- build.ps1
+++ build.ps1
@@ -1,1 +1,11 @@
+# Build front end
+cd ./ClientApp
+npm run build
+cd..
+
+# Copy frontend to dotnet app
 Copy-Item -Path ($PSScriptRoot+"\ClientApp\build\*") -Destination ($PSScriptRoot+"\wwwroot") -Recurse -force
+
+# Build dotnet
+dotnet restore
+dotnet build
