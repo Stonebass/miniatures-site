23e8e697746933da62c6bfe086314041caf029c1
Johan Stenberg <johanhenrystenberg@hotmail.com>
2018-09-27 22:42:54 +0200

    fixed build script and index page to remove hash from npm build

--- build.ps1
+++ build.ps1
@@ -1,7 +1,13 @@
-# Build front end
+# # Build front end
 cd ./ClientApp
 npm run build
 cd..
 
 # Copy frontend to dotnet app
-Copy-Item -Path ($PSScriptRoot+"\ClientApp\build\*") -Destination ($PSScriptRoot+"\wwwroot") -Recurse -force
+Copy-Item -Path ($PSScriptRoot + "\ClientApp\build\*") -Destination ($PSScriptRoot + "\wwwroot") -Recurse -force
+
+Get-ChildItem ($PSScriptRoot + "\wwwroot\static\js\*.js") | Rename-Item -NewName "main.js"
+Get-ChildItem ($PSScriptRoot + "\wwwroot\static\js\*.js.map") | Rename-Item -NewName "main.js.map"
+
+Get-ChildItem ($PSScriptRoot + "\wwwroot\static\css\*.css") | Rename-Item -NewName "main.css"
+Get-ChildItem ($PSScriptRoot + "\wwwroot\static\css\*.css.map") | Rename-Item -NewName "main.css.map"
