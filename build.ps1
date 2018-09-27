958b394ab333a9db8f5cb6da005174b34ca81f5b
Johan Stenberg <johanhenrystenberg@hotmail.com>
2018-09-27 20:11:09 +0200

    fixed build script

--- build.ps1
+++ build.ps1
@@ -5,7 +5,3 @@ cd..
 
 # Copy frontend to dotnet app
 Copy-Item -Path ($PSScriptRoot+"\ClientApp\build\*") -Destination ($PSScriptRoot+"\wwwroot") -Recurse -force
-
-# Build dotnet
-dotnet restore
-dotnet build
