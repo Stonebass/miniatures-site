ea38ef31e80e66e5481dfe6733e42d61bbd54106
Johan Stenberg <johanhenrystenberg@hotmail.com>
2018-09-28 23:37:15 +0200

    Messed more with the build script

--- build.ps1
+++ build.ps1
@@ -4,15 +4,18 @@ npm run build
 cd..
 
 # Copy frontend to dotnet app
-if (-Not (Test-Path ($PSScriptRoot + "\wwwroot")))
-{
-     md -path ($PSScriptRoot + "\wwwroot")
+if (-Not (Test-Path ($PSScriptRoot + "\wwwroot"))) {
+  md -path ($PSScriptRoot + "\wwwroot")
 }
 Copy-Item -Path ($PSScriptRoot + "\ClientApp\build\*") -Destination ($PSScriptRoot + "\wwwroot") -Recurse -force
 
+ls
+cd ./wwwroot
+ls
+cd..
 
-Get-ChildItem ($PSScriptRoot + "\wwwroot\js\*.js") | Rename-Item -NewName "main.js"
-Get-ChildItem ($PSScriptRoot + "\wwwroot\js\*.js.map") | Rename-Item -NewName "main.js.map"
+Get-ChildItem ($PSScriptRoot + "\wwwroot\static\js\*.js") | Rename-Item -NewName "main.js"
+Get-ChildItem ($PSScriptRoot + "\wwwroot\static\js\*.js.map") | Rename-Item -NewName "main.js.map"
 
-Get-ChildItem ($PSScriptRoot + "\wwwroot\css\*.css") | Rename-Item -NewName "main.css"
-Get-ChildItem ($PSScriptRoot + "\wwwroot\css\*.css.map") | Rename-Item -NewName "main.css.map"
+Get-ChildItem ($PSScriptRoot + "\wwwroot\static\css\*.css") | Rename-Item -NewName "main.css"
+Get-ChildItem ($PSScriptRoot + "\wwwroot\static\css\*.css.map") | Rename-Item -NewName "main.css.map"
