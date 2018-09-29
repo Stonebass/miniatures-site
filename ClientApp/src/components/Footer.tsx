c7f28c984af31f6c9ac5454cc232e74d4478907e
Johan Stenberg <johanhenrystenberg@hotmail.com>
2018-09-29 16:24:40 +0200

    Added github icon and link

--- ClientApp/src/components/Footer.tsx
+++ ClientApp/src/components/Footer.tsx
@@ -5,9 +5,17 @@ class Footer extends React.Component {
   public render() {
     return (
       <div className="footer">
-        <div className="icons">
+        <div className="icon">
           <a href="https://www.instagram.com/stonebassminiatures/">
             <img src="/instagramicon.svg" />
+          </a>
+          {/* </div>
+        <div className="icon"> */}
+          <a
+            href="https://github.com/Stonebass/miniatures-site"
+            style={{ marginLeft: "5px" }}
+          >
+            <img src="/githubicon.svg" />
           </a>
         </div>
       </div>
